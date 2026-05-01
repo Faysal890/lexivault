381bda35272b224d716b7dcf38fff617db34bccb
Khalilur Rahman Faysal <117637433+Faysal890@users.noreply.github.com>
2026-05-01 16:24:27 +0600

    fixed middleware issue

--- middleware.ts
+++
@@ -1,5 +0,0 @@
-export { default } from "next-auth/middleware";
-
-export const config = {
-  matcher: ["/dashboard/:path*", "/words/:path*", "/quiz/:path*", "/stats/:path*", "/profile/:path*", "/admin/:path*", "/store/:path*"],
-};
